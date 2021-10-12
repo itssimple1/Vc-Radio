8b170c9c951833ce9eb38ae1752bd728459e465e
itssimple1 <91212696+itssimple1@users.noreply.github.com>
2021-10-13 00:39:19 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -46,7 +46,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[© @its_simple1]" 
+            performer = f"[@its_simple1]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
